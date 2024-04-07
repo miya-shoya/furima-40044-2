0e1fdc5d68117178ff28b62646f0fdd1840a0a2c
miya-shoya <miya19981215@gmail.com>
2024-04-07 19:43:40 +0900

    商品購入機能の修正６

--- app/models/buy_address.rb
+++ app/models/buy_address.rb
@@ -16,6 +16,14 @@ class BuyAddress
 
   def save
     buy = Buy.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_code: postal_code, region_id: region_id, city: city, house_number: house_number, phone_number: phone_number, buy_id: buy.id)
+    Address.create(
+      postal_code: postal_code, 
+      region_id: region_id, 
+      city: city, 
+      house_number: house_number, 
+      phone_number: phone_number, 
+      building_name: building_name, 
+      buy_id: buy.id
+    )
   end
-end
+  d

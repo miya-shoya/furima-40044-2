aceae734389b1fc6fd416338d406c2e228aff668
miya-shoya <miya19981215@gmail.com>
2024-04-09 19:32:15 +0900

    フリマアプリ挙動確認修正２

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,4 +1,5 @@
 class BuysController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
   before_action :set_item, only: [:index, :create]
 
   def index
--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,14 +1,19 @@
-
-window.addEventListener('DOMContentLoaded', () => {
-  const  priceInput = document.getElementById("item-price");
+const calculatePrice = () => {
+  const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    const addTaxDom = document.getElementById("add-tax-price");
-    const fee = inputValue / 10;
-    addTaxDom.innerHTML = fee;
+    if (inputValue) {
+      const addTaxDom = document.getElementById("add-tax-price");
+      const fee = Math.floor(inputValue / 10);
+      addTaxDom.innerHTML = fee;
 
-    const profitDom = document.getElementById("profit");
-    const profit = inputValue - fee;
-    profitDom.innerHTML = profit;
+      const profitDom = document.getElementById("profit");
+      const profit = inputValue - fee;
+      profitDom.innerHTML = profit;
+    }
   })
-});
+};
+
+window.addEventListener("turbo:load", calculatePrice);
+window.addEventListener("turbo:render", calculatePrice);
+
